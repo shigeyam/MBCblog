d521b642f44b80d927cace2df59aee9bd029bac6
shigeyam <shigeyam1212@gmail.com>
2022-08-30 11:24:15 +0900

    Add

--- MBCblog/settings.py
+++ MBCblog/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATE_DIR = BASE_DIR / "Templates"
+TEMPLATE_DIR = BASE_DIR / "templates"
 STATIC_DIR = BASE_DIR / "static"
 MEDIA_DIR = BASE_DIR / "media"
 
