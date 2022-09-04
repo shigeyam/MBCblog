c33f9625e61c8d530436d0a75fe115f1417898e3
shigeyam <shigeyam1212@gmail.com>
2022-09-05 07:56:18 +0900

    Add

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Meeting, ImageUpload
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Meeting)
+admin.site.register(ImageUpload)
