5133ec00dc009ed1df8d9227b0ec8ad8a1069ba0
shigeyam <shigeyam1212@gmail.com>
2022-09-09 10:38:12 +0900

    Admin

--- MBCblog/urls.py
+++ MBCblog/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth.models import Group
+
+admin.site.site_title = "森田塾　管理サイト"
+admin.site.site_header = "森田塾　管理サイト"
+admin.site.index_title = "メニュー"
+admin.site.unregister(Group)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
