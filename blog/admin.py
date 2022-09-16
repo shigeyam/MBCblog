3e64b8ea06faacfd58daef7da057aa6f2cc55450
shigeyam <shigeyam1212@gmail.com>
2022-09-16 21:12:22 +0900

    Add CommentAdmin and MeetingAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,16 @@ from django_summernote.admin import SummernoteModelAdmin
 
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('body')
+    list_display = ('slug', 'title', 'posted_date')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'posted_date')
+
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'posted_date')
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
-admin.site.register(Meeting)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Meeting, MeetingAdmin)
 admin.site.register(ImageUpload)
 
